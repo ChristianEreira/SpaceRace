033a9cdbc5aa6e0fefd8f41024d17d5d3160a040
Christian Ereira <contact@chrise.dev>
2021-08-29 15:12:25 +0100

    Incorportate colour into fillRect function +loop optimisations

--- a.js
+++ a.js
@@ -1,10 +1,12 @@
-let ctx;
+let ctx, i;
+let size = 500;
 
 let changeFill = col => {
     ctx.fillStyle = col;
 };
 
-let fillRect = (x, y, width, height) => {
+let fillRect = (x, y, width, height, col) => {
+    if (col) { changeFill('#' + col); }
     ctx.fillRect(x, y, width, height);
 };
 
@@ -12,28 +14,18 @@ let rand = (min, max) => min + Math.random() * (max - min);
 
 window.onload = () => {
     ctx = document.querySelector("#a").getContext('2d');
-    let size = 500;
 
-    changeFill('#38B7FF');
-    fillRect(0, 0, size, size);
+    fillRect(0, 0, size, size, '38B7FF');
 
-    let i = 0;
-    while (i < 500) {
-        changeFill('#33A6FF');
-        fillRect(i, size - 30 - rand(30, 99), rand(30, 99), 99 + 30);
-        i += 30;
+    for (i = 0; i < 500; i += 30) {
+        fillRect(i, size - 30 - rand(30, 99), rand(30, 99), 99 + 30, '33A6FF');
     }
 
-    changeFill('#007C1B');
-    fillRect(0, size - 18, size, 18);
+    fillRect(0, size - 18, size, 18, '007C1B');
+    fillRect(0, size - 18 - 8, size, 8, '008A1E');
+    fillRect(0, size - 18 - 8 - 12, size, 12, '00AC2E');
 
-    changeFill('#008A1E');
-    fillRect(0, size - 18 - 8, size, 8);
-
-    changeFill('#00AC2E');
-    fillRect(0, size - 18 - 8 - 12, size, 12);
-
-    for (let i = 0; i < 500; i+=10) {
+    for (i = 0; i < 500; i += 10) {
         ctx.beginPath();
         ctx.moveTo(i, size - 18 - 8 - 12);
         ctx.lineTo(i + 10, size - 18 - 8 - 12);
